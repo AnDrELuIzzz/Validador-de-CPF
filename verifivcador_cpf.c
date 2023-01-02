4acfb46fd6383f3b2b570c0b4a7d4c9bb86b2039
Andre Luiz <luizzz.z127@gmail.com>
2023-01-02 18:30:11 -0300

    Co-authored-by: Gabrielly Baião <GabriellyBaiao@users.noreply.github.com>

--- verifivcador_cpf.c
+++ verifivcador_cpf.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <locale.h>
-void estado(int digito)
-{
-    switch (digito)
-    {
-    case 0:
-        printf("Rio Grande do Sul");
-        break;
-    case 1:
-        printf("Distrito Federal, Goiás, Mato Grosso, Mato Grosso do Sul e Tocantins");
-        break;
-    case 2:
-        printf("Amazonas, Pará, Roraima, Amapá, Acre e Rondônia");
-        break;
-    case 3:
-        printf("Ceará, Maranhão e Piauí");
-        break;
-    case 4:
-        printf("Paraíba, Pernambuco, Alagoas e Rio Grande do Norte");
-        break;
-    case 5:
+#include <string.h>
 
-        printf("Bahia e Sergipe");
-        break;
-    case 6:
-        printf("Minas Gerais");
-        break;
-    case 7:
-        printf("Rio de Janeiro e Espírito Santo");
-        break;
-    case 8:
-        printf("São Paulo");
-        break;
-    case 9:
-        printf("Paraná e Santa Catarina");
-        break;
-    }
+void imprimeRegiao(int cpf[])
+{
+    if (cpf[8] == 0)
+        printf("\nRio Grande do Sul");
+    if (cpf[8] == 1)
+        printf("\nDistrito Federal, Goiás, Mato Grosso, Mato Grosso do Sul e Tocantins");
+    if (cpf[8] == 2)
+        printf("\nAmazonas, Pará, Roraima, Amapá, Acre e Rondônia");
+    if (cpf[8] == 3)
+        printf("\nCeará, Maranhão e Piauí");
+    if (cpf[8] == 4)
+        printf("\nParaíba, Pernambuco, Alagoas e Rio Grande do Norte");
+    if (cpf[8] == 5)
+        printf("\nBahia e Sergipe");
+    if (cpf[8] == 6)
+        printf("\nMinas Gerais");
+    if (cpf[8] == 7)
+        printf("\nRio de Janeiro e Espírito Santo");
+    if (cpf[8] == 8)
+        printf("\nSão Paulo");
+    if (cpf[8] == 9)
+        printf("\nParaná e Santa Catarina");
 }
+
 bool todosRepetidos(int cpf[])
 {
     int i;
@@ -49,51 +37,46 @@ bool todosRepetidos(int cpf[])
     return true;
 }
 
+int digitoVerificador(int cpf[], int mult[], int N)
+{
+    int i, soma = 0;
+    for (i = 0; i < N; i++)
+    {
+        soma = soma + cpf[i] * mult[i];
+    }
+    int resto = ((soma * 10) % 11) % 10;
+    return resto;
+} // fim da função
+
 int main()
 {
     int cpf[11];
     int m1[9] = {10, 9, 8, 7, 6, 5, 4, 3, 2};
     int m2[10] = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-    int i;
-    printf("Entre com os 11 digitos do CPF:");
-    for (i = 0; i < 11; i++)
+    int i, j = 0, tam;
+    char entrada[50];
+    printf("Entre com o CPF: ");
+    fgets(entrada, 50, stdin);
+    tam = strlen(entrada);
+    for (i = 0; i < tam; i++)
     {
-        scanf("%d", &cpf[i]);
+        if (entrada[i] >= 48 && entrada[i] <= 57)
+        {
+            cpf[j] = entrada[i] - 48;
+            j++;
+        }
     }
-    // Cálculo do primeiro dígito verificador
-    int soma = 0;
-    for (i = 0; i < 9; i++)
-    {
-        soma = soma + cpf[i] * m1[i];
-    }
-    int resto1 = ((soma * 10) % 11) % 10;
 
-    if (resto1 == cpf[9])
+    if (digitoVerificador(cpf, m1, 9) == cpf[9] &&
+        digitoVerificador(cpf, m2, 10) == cpf[10] &&
+        !todosRepetidos(cpf))
     {
-        printf("\nPrimeiro verificador valido.");
+        printf("\nCPF válido.");
+        imprimeRegiao(cpf);
     }
     else
     {
-        printf("\nErro no primeiro verificador.");
-    }
-
-    // Cálculo do segundo dígito verificador
-    soma = 0;
-
-    for (i = 0; i < 10; i++)
-    {
-        soma = soma + cpf[i] * m2[i];
-    }
-    int resto2 = ((soma * 10) % 11) % 10;
-
-    if (resto1 == cpf[9] && resto2 == cpf[10] && !todosRepetidos(cpf))
-    {
-        printf("\nCPF valido.");
-        estado(cpf[8]);
-    }
-    else
-    {
-        printf("\n Erro no CFP");
+        printf("\nErro no CPF.");
     }
     return 0;
 }
